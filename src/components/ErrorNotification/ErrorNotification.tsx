7f3cd0a1516ad8e568124ce448a32d3a462c4bf6
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-03-28 13:45:51 +0200

    split App into components , add User_ID

---
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,20 @@
+export const ErrorNotification = () => {
+  return (
+    <div
+      data-cy="ErrorNotification"
+      className="notification is-danger is-light has-text-weight-normal"
+    >
+      <button data-cy="HideErrorButton" type="button" className="delete" />
+      {/* show only one message at a time */}
+      Unable to load todos
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  );
+};
